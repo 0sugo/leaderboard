57e5b70fce99b1ad636db582db0a4a4fac49fcc4
0sugo <josugo26@gmail.com>
2023-04-14 14:57:25 +0300

    Update index.js to use modules

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+
 import './style.css';
+import Leaderbord from './modules/Leaderboard';
+// post.js
+const updater = document.querySelector('.submit');
+updater.addEventListener('click', (event) => {
+  event.preventDefault();
+  Leaderbord.generate();
+});
+// get.js
+const retriever = document.querySelector('.retriever');
+retriever.addEventListener('click', (event) => {
+  event.preventDefault();
+  Leaderbord.originalInserter();
+  Leaderbord.updateUi();
+});
