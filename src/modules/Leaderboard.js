c7b1b45d2ff0851c02fe619f6f5f50b792ebf272
0sugo <josugo26@gmail.com>
2023-04-14 14:55:46 +0300

    Add post to UI function

--- src/modules/Leaderboard.js
+++ src/modules/Leaderboard.js
@@ -10,7 +10,19 @@ class Leaderbord {
       }).then((response) => response.json()); // { resut: [{"kaleb": 77}, {"osugo": 77}]}
       return response;
     };
-    
+    static updateUi = async () => {
+      const insert = document.querySelector(".insert");
+      insert.innerHTML = "";
+      const response = await Leaderbord.originalInserter();
+      console.log("response is: ", response);
+      response.result.forEach((element) => {
+        const div = document.createElement("tr");
+        div.innerHTML = `
+                  <td>${element.user} : ${element.score}</td>
+                  `;
+        insert.appendChild(div);
+      });
+    };
     static generate = async () => {
       let playerName = document.querySelector("#name").value;
       let playerScore = document.querySelector("#score").value;
